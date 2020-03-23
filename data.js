683b290683657e899f3a8ca59109fb0fffe2b648
Sushma Goutam <write2sushma@hotmail.com>
2020-03-23 22:13:01 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,33 @@
+const main = {
+  name: 'Sushma Goutam',
+  mail: 'write2sushma@gmail.com',
+  img: 'https://avatars2.githubusercontent.com/u/6429329?s=400&u=5990a6896990a005af7a58f480de0854fb2a2ff8&v=4',
+  role: ['Senior Software Developer'],
+  // Refer https://ionicons.com/ and put the icon name inside iconName key
+  connects: [
+    {
+      name: 'Home',
+      iconName: 'logo-firefox',
+      link: 'https://sivanesh-s.github.io'
+    },
+    {
+      name: 'Github',
+      iconName: 'logo-github',
+      link: 'https://github.com/write2sushma'
+    },
+    {
+      name: 'Linkedin',
+      iconName: 'logo-linkedin',
+      link: 'https://www.linkedin.com/in/sushmagoutam/'
+    },
+    {
+      name: 'Twitter',
+      iconName: 'logo-twitter',
+      link: 'https://twitter.com/sushmag'
+    }    
+  ],
+  links: [
+    { name: 'Projects', link: '#projects' },
+    { name: 'Blogs', link: '#blogs' }
+  ]
+};
